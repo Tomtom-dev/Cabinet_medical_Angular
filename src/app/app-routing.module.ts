52688bcb5fa1b77a45bc3b66ef886f083f61d1bc
thomas Heddadji <thomasheddadji0@gmail.com>
2021-10-01 15:42:18 +0200

    add path to the page for patient infirmiere and deplacement and style the button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeplacementComponent } from './deplacement/deplacement.component';
+import { InfirmiereComponent } from './infirmiere/infirmiere.component';
+import { PatientComponent } from './patient/patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"patient", component: PatientComponent},
+  {path:"infirmiere", component: InfirmiereComponent},
+  {path:"deplacement", component: DeplacementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
