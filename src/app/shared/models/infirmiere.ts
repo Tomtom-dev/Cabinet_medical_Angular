a4eea10183b9f64bb84b540eef3e9d609b58dbe3
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-10-02 14:16:40 +0200

    complete the model and service for infirmiere

---
+++ src/app/shared/models/infirmiere.ts
@@ -0,0 +1,8 @@
+export interface Infirmiere{
+    id: number,
+    numero_professionnel: number,
+    nom:String,
+    prenom:String,
+    telpro:number,
+    tel_perso:number
+}
--- src/app/shared/service/infirmiere.service.ts
+++ src/app/shared/service/infirmiere.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Infirmiere } from '../models/infirmiere';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,7 @@ export class InfirmiereService {
 
   constructor(private http: HttpClient) { }
 
-  public getInfirmiere(): Observable <Infirmiere[]>
+  public getInfirmiere(): Observable <Infirmiere[]>{
+    return this.http.get<Infirmiere[]>(`${environment.url}/infirmieres`)
+  }
 }
